a1ef1aa3d2e0287ee830ae06bd1d5d2845acc5f0
Jeon-HS4 <jin0004@tukorea.ac.kr>
2023-03-28 02:12:35 +0900

    Spring Boot 기본 세팅(~2-02)

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -1,2 +1,15 @@
-package com.mysite.sbb;public class MainController {
+package com.mysite.sbb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/sbb")
+    @ResponseBody
+    public String index(){
+        return "안녕하세 sbb에 오신것을 환영합니다";
+    }
 }
