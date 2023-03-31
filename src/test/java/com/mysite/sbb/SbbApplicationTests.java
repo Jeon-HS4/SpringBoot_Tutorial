10515c316e1cc9c0a8c9474fc7c50e59a66821cb
Jeon-HS4 <jin0004@tukorea.ac.kr>
2023-03-31 13:40:18 +0900

    Spring Boot 기본 요소 익히기!(2-02 ~ 2-15)

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -1,13 +1,67 @@
 package com.mysite.sbb;
 
+import com.mysite.sbb.question.QuestionRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class SbbApplicationTests {
 
+    @Autowired
+    private QuestionRepository questionRepository;
     @Test
-    void contextLoads() {
+    void testJpa() {
+        /*
+        1. 데이터 저장하기
+        Question q1 = new Question();
+        q1.setSubject("sbb가 무엇인가요?");
+        q1.setContent("sbb에 대해서 알고 싶습니다.");
+        q1.setCreateDate(LocalDateTime.now());
+        this.questionRepository.save(q1);   // 첫번째 질문 저장
+
+        Question q2 = new Question();
+        q2.setSubject("스프링부트 모델 질문입니다.");
+        q2.setContent("id는 자동으로 생성되나요?");
+        q2.setCreateDate(LocalDateTime.now());
+        this.questionRepository.save(q2);
+
+        2. 데이터 조회하기
+        List<Question> all = this.questionRepository.findAll();
+        assertEquals(2,all.size());
+
+        Question q = all.get(0);
+        assertEquals("sbb가 무엇인가요?", q.getSubject());
+
+        3. ID로 데이터 검색
+        Optional<Question> oq = this.questionRepository.findById(1);
+        if(oq.isPresent()) {
+            Question q = oq.get();
+            assertEquals("sbb가 무엇인가요?", q.getSubject());
+        }
+
+        4. subject값으로 데이터 조회
+        Question q = this.questionRepository.findBySubject("sbb가 무엇인가요?");
+        assertEquals(1, q.getId());
+
+        5. 제목과 내용을 함께 조회
+        Question q = this.questionRepository.findBySubjectAndContent(
+                "sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다."
+        );
+        assertEquals(1, q.getId());
+
+        6. findBySubjectLike
+        List<Question> qList = this.questionRepository.findBySubjectLike("sbb%");
+        Question q = qList.get(0);
+        assertEquals("sbb가 무엇인가요?",q.getSubject());
+
+
+         */
+
+
+
     }
 
 }
